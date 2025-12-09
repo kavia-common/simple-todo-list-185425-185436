055a1a356d04a1e7d1a161cb08ea77fad1766540
Kavia AI <production@kavia.ai>
2025-12-09 15:29:03 +0000

    Checkpoint 5 - cg1fbf2a3b

---
+++ todo_frontend/hello_world.js
@@ -0,0 +1,7 @@
+/* PUBLIC_INTERFACE */
+/**
+ * Minimal standalone script that logs "Good morning" to the console.
+ * This file is independent and does not integrate with the React app bundle.
+ */
+// eslint-disable-next-line no-console
+console.log('Good morning');
